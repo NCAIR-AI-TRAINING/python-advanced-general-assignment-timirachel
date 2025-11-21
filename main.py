0df266965f8c7c6e3f2a9f254615134b6cab0d40
Alagba Timipre <timirachel44@gmail.com>
2025-11-21 12:58:54 +0100

    added 5 minute waiting rule

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 
 class DuplicateVisitorError(Exception):
@@ -7,7 +7,8 @@ class DuplicateVisitorError(Exception):
         super().__init__(f"Visitor '{visitor}' has already visited." )
 
 class EarlyEntryError(Exception):
-    pass
+    def __init__(self):
+        super().__init__("A 5-minute wait is required between different visitors.")
 
 FILENAME = "visitors.txt"
 
@@ -25,12 +26,27 @@ def get_last_visitor():
         lines = f.readlines()
         if lines:
             return lines[-1].strip().split(" | ")[0] 
+        
+def get_last_timestamp():
+    with open(FILENAME, "r") as f:
+        lines = f.readlines()
+        if lines:
+            return lines[-1].strip().split(" | ")[1]
 
 def add_visitor(visitor_name):
     last_visitor = get_last_visitor()
+    last_timestamp = get_last_timestamp()
 
     if visitor_name == last_visitor:
         raise DuplicateVisitorError(visitor_name)
+    
+    if last_timestamp:
+        last_time = datetime.fromisoformat(last_timestamp)
+        current_time = datetime.now()
+        time_difference = current_time - last_time
+
+        if time_difference < timedelta(minutes = 5):
+            raise EarlyEntryError()
     
     timestamp = datetime.now().isoformat()
     entry = f"{visitor_name} | {timestamp}\n"
